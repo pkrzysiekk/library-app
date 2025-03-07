658e64e621865c22ac04f58bdaee66ac408d4f4e
pkrzysiek <pkrzysiek24@gmail.com>
2025-03-07 21:42:28 +0100

    Add Client controller with views

---
+++ Wypozyczalnia/Controllers/ClientController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wypozyczalnia.Data;
+using Wypozyczalnia.Models;
+
+namespace Wypozyczalnia.Controllers
+{
+    public class ClientController : Controller
+    {
+        private LibraryContext context;
+        public ClientController(LibraryContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var clients = context.Clients.Select(x => x).ToList();
+            return View(clients);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var client = await context.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            context.Clients.Remove(client);
+            await context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            [Bind("Name,LastName")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(client);
+                await context.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return View(client);
+        }
+    
+        public async Task<IActionResult> Edit(int id)
+        {
+            var client = await context.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return View(client);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Update(client);
+                await context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(client);
+        }
+
+    }
+}
--- Wypozyczalnia/Models/Client.cs
+++ Wypozyczalnia/Models/Client.cs
@@ -1,13 +1,18 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Wypozyczalnia.Models
 {
     public class Client
     {
         public int Id { get; set; }
-        public required string Name { get; set; }
-        public required string LastName { get; set; }
 
-        public ICollection<Rental> Rentals { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+
+        public string LastName { get; set; }
+
+        public ICollection<Rental>? Rentals { get; set; }
     }
 }

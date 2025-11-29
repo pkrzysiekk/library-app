a7501d30bef120239543a5958f446b1f64c633b7
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-29 13:45:29 +0100

    Create ReCaptchaValidator.cs

---
+++ Wypozyczalnia/Validators/reCAPTCHA/ReCaptchaValidator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using Wypozyczalnia.Models;
+
+namespace Wypozyczalnia.Validators.reCAPTCHA;
+
+public class ReCaptchaValidator
+{
+    private readonly string _secret;
+    private readonly HttpClient _httpClient;
+
+    public ReCaptchaValidator(IOptions<ReCaptchaSettings> settings)
+    {
+        _secret = settings.Value.SecretKey;
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<bool> IsValid(string token)
+    {
+        var response = await _httpClient.PostAsync(
+            $"https://www.google.com/recaptcha/api/siteverify?secret={_secret}&response={token}",
+            null);
+
+        var json = await response.Content.ReadAsStringAsync();
+        return json.Contains("\"success\": true");
+    }
+}

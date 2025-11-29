7fce1bafd4eaf871f6bdcd362f5232d874a85d58
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-11-29 13:50:55 +0100

    Create ForgotPassword.cshtml.cs

---
+++ Wypozyczalnia/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -0,0 +1,103 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Wypozyczalnia.Validators.reCAPTCHA;
+
+namespace Wypozyczalnia.Areas.Identity.Pages.Account
+{
+    public class ForgotPasswordModel : PageModel
+    {
+        [BindProperty]
+        public string RecaptchaToken { get; set; }
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IEmailSender _emailSender;
+        private readonly ReCaptchaValidator _reCaptchaValidator;
+
+        public ForgotPasswordModel(UserManager<IdentityUser> userManager, IEmailSender emailSender, ReCaptchaValidator validator)
+        {
+            _userManager = userManager;
+            _emailSender = emailSender;
+            _reCaptchaValidator = validator;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            RecaptchaToken = Request.Form["g-recaptcha-response"];
+
+            if (string.IsNullOrEmpty(RecaptchaToken))
+            {
+                ModelState.AddModelError(string.Empty, "Potwierdź, że nie jesteś robotem.");
+                return Page();
+            }
+
+            var isHuman = await _reCaptchaValidator.IsValid(RecaptchaToken);
+
+            if (!isHuman)
+            {
+                ModelState.AddModelError(string.Empty, "Nie udało się zweryfikować reCAPTCHA.");
+                return Page();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(Input.Email);
+                if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+                {
+                    // Don't reveal that the user does not exist or is not confirmed
+                    return RedirectToPage("./ForgotPasswordConfirmation");
+                }
+
+                // For more information on how to enable account confirmation and password reset please
+                // visit https://go.microsoft.com/fwlink/?LinkID=532713
+                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callbackUrl = Url.Page(
+                    "/Account/ResetPassword",
+                    pageHandler: null,
+                    values: new { area = "Identity", code },
+                    protocol: Request.Scheme);
+
+                await _emailSender.SendEmailAsync(
+                    Input.Email,
+                    "Reset Password",
+                    $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                return RedirectToPage("./ForgotPasswordConfirmation");
+            }
+
+            return Page();
+        }
+    }
+}
